c005ae2ad042a1bd52ce26762e9c365276bf09f6
esantb <e.saint.bou@gmail.com>
2023-02-16 14:31:41 -0600

    correct guessing display works

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,18 @@
 def play_game():
-    pass
+
+    word = 'apple'
+    wrong_guesses = ['b', 'd']
+    right_guesses = ['a', 'p', 'e']
+
+# empty string
+    game_board = ' '
+    for letter in word:
+        if letter in right_guesses:
+            game_board += letter + ' '
+        else:
+            game_board += '_ '
+
+    print(game_board)
 
 
 if __name__ == "__main__":
