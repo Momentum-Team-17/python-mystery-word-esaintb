5ba5105569c05871b868244976a91111930b6181
esantb <e.saint.bou@gmail.com>
2023-02-19 10:29:10 -0600

    connected mystery word to game function with _

--- mystery_word.py
+++ mystery_word.py
@@ -1,39 +1,55 @@
+import random
+
 
 # FUNCTION FOR OPENING FILE AND READING IT
-# defining the function w/o arguments because it's localized
 def open_file(file):
     # opening the file in read mode and will auto close it too
     with open(file) as my_file:
-        # reading the file on the right then in data it makes it readable for humans
+        # reading the file on the right then in data (on left) it makes it readable for humans
         data = my_file.read()
+        # formatting words into a list
         format_data = data.split()
+        # catching formatted data
         return format_data
 
 
-print(open_file('words.txt'))
-
-# WILL NEED TO CALL FILE FUNCTION
-# open_file('words.txt')
-
-
-# DISPLAY CORRECT USER GUESS
-
-#     word = 'apple'
-#     wrong_guesses = ['b', 'd']
-#     right_guesses = ['a', 'p', 'e']
-
-# # empty string
-#     game_board = ' '
-#     for letter in word:
-#         if letter in right_guesses:
-#             game_board += letter + ' '
-#         else:
-#             game_board += '_ '
-
-#     print(game_board)
+# RANDOM WORD CHOICE FUNCTION
+def random_word(list):
+    # selecting a random word from a blank list
+    word = random.choice(list)
+    # catching the random word
+    return word
 
 
-# def play_game():
+def play_game():
+    # links random word choice function to the file in question
+    chosen_word = random_word(open_file('words.txt'))
+    # defining random word length
+    word_length = len(chosen_word)
+    # attributing underscores to the length
+    underscore = '_ ' * word_length
 
+    print(chosen_word)
+    print(f'Your word has {word_length} letters.')
+    print(underscore)
+
+
+# 8 wrong guesses loop
+# count = 0
+# while wrong_guesses <= 8:
+#     play_game()
+#     if wrong_guesses == 8:
+#         display random_word
+#         print("womp womp")
+#         break
+#     else:
+#         display random_word
+#         print("Winner winner chicken dinner")
+# PLAY AGAIN LOOP
+# play_again = input("Do you want to play again Y/N?\n")
+# if play_again == 'Y':
+#     play_game()
 if __name__ == "__main__":
     play_game()
+
+# python mystery_word.py
